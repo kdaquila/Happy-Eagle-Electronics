8238f74b51d999f4d46cbe342dd0d2e9dbb40a52
Kenneth D'Aquila <kfd182@gmail.com>
2019-05-08 23:19:14 -0500

    Refactored HTML, CSS, and JS. Site Working.

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,49 @@
-$('main .primarySection').each(function(){
-    var $this = $(this);
-    var $figures = $this.find('figure');
-    var $moveButons = $this.find('.primarySection-moveButton');
-    var $playButton = $this.find('.primarySection-playButton');
-    var $pauseButton = $this.find('.primarySection-pauseButton');
-    var $nextButtons = $this.find('.primarySection-nextButton');
-    var $prevButtons = $this.find('.primarySection-prevButton');
-    var nFigures = $figures.length;
+// define the CSS selectors
+var imageSliderSelector = ".primaryFigureSection";
+var imageSetSelector = ".primaryFigureSection-figureSet";
+var imageItemSelector = ".primaryFigureSection-figure";
+var moveButtonSelector = ".primaryFigureSection-moveButton";
+var playButtonSelector = ".primaryFigureSection-playButton";
+var pauseButtonSelector = ".primaryFigureSection-pauseButton";
+var nextButtonSelector = ".primaryFigureSection-nextButton";
+var prevButtonSelector = ".primaryFigureSection-prevButton";
+var nextClass = "next";
+var prevClass = "prev";
+var hiddenClass = "hidden";
+var markedClass = "marked";
+
+// setup image slider functionality on each image slider
+$(imageSliderSelector).each(function(){
+
+    // store the reference
+    var $slider = $(this);
+
+    // find all the components
+    var $imageSet = $slider.find(imageSetSelector);
+    var $imageItems = $slider.find(imageItemSelector);
+    var $moveButons = $slider.find(moveButtonSelector);
+    var $playButton = $slider.find(playButtonSelector);
+    var $pauseButton = $slider.find(pauseButtonSelector);
+    var $nextButtons = $slider.find(nextButtonSelector);
+    var $prevButtons = $slider.find(prevButtonSelector);
+
+    // number of image items
+    var nImageItems = $imageItems.length;
+
+    // define indices
     var currIndex = 0;
     var nextIndex = 1;
-    var prevIndex = nFigures-1;
-    var timeout;
-    var isAutoPlay = false;
+    var prevIndex = nImageItems-1;
 
-    var currWidth = $('main').css('width');
-    $('.primarySection-prevState').css({width: currWidth, right: currWidth});
-    $('.primarySection-nextState').css({width: currWidth, left: currWidth})
+    // timeout object
+    var timeout;
+
+    // auto play
+    var isAutoPlay = false;
+    var autoPlayDelay_ms = 2000;
+
+    // slide duration
+    var slideDuration_ms = 300;
 
     function next() {
         move(nextIndex);
@@ -25,127 +53,100 @@ $('main .primarySection').each(function(){
         move(prevIndex);
     }
 
-    function move(newIndex) {
-        
-        var currWidth = $this.css('width');
-        var delay = 300;
-
-        function update()
-        {
-            $figures.eq(newIndex).removeClass('primarySection-hiddenState');
-            $figures.eq(newIndex).removeClass('primarySection-prevState');
-            $figures.eq(newIndex).removeClass('primarySection-nextState');
-            $figures.eq(newIndex).addClass('primarySection-currState');
-            $figures.eq(currIndex).removeClass('primarySection-currState');
-            $figures.eq(currIndex).removeClass('primarySection-prevState');
-            $figures.eq(currIndex).removeClass('primarySection-nextState');
-            $figures.eq(currIndex).addClass('primarySection-hiddenState');
-
-            $moveButons.find('circle').css({fill: 'transparent'});
-            $moveButons.eq(newIndex).find('circle').css({fill: 'white'});
-
-            currIndex = newIndex;
-            nextIndex = (currIndex+1)%nFigures;
-            prevIndex = ((currIndex-1)%nFigures + nFigures)%nFigures;                
-            
-            $figures.css({right: '', left: '', width: ''});
-        }
-
-        if ($figures.eq(newIndex).is(':animated') || 
-            $figures.eq(currIndex).is(':animated') ||
-            newIndex == currIndex) {
-                return;
-            }
-        else if (newIndex > currIndex) {       
-            $figures.eq(newIndex).removeClass('primarySection-hiddenState');
-            $figures.eq(newIndex).removeClass('primarySection-prevState');
-            $figures.eq(newIndex).removeClass('primarySection-nextState');
-            $figures.eq(newIndex).addClass('primarySection-nextState');
-            $figures.eq(newIndex).css({left: currWidth, width: currWidth});
-            $figures.eq(currIndex).animate({right: currWidth}, delay);
-            $figures.eq(newIndex).animate({left: 0}, delay, update);
-        } else if (newIndex < currIndex) {
-            $figures.eq(newIndex).removeClass('primarySection-hiddenState');
-            $figures.eq(newIndex).removeClass('primarySection-prevState');
-            $figures.eq(newIndex).removeClass('primarySection-nextState');
-            $figures.eq(newIndex).addClass('primarySection-prevState');
-            $figures.eq(newIndex).css({right: currWidth, width: currWidth});
-            $figures.eq(currIndex).animate({left: currWidth}, delay);
-            $figures.eq(newIndex).animate({right: 0}, delay, update);
-        }    
-        
-        if (isAutoPlay==true)
-        {
-            advance();
-        }
-    }
-
     function advance() {        
-        var delay = 2000;
         clearTimeout(timeout);
-        timeout = setTimeout(function(){next();}, delay); 
+        timeout = setTimeout(function(){next();}, autoPlayDelay_ms); 
     }
 
-    // setup move buttons
-    $moveButons.each(function(index){
-        $(this).on('click', function(){
-            move(index);
-        });
-    });
-    $moveButons.eq(currIndex).find('circle').css({fill: 'white'});
-
-    // setup next buttons
-    $nextButtons.each(function(index){
-        $(this).on('click', function(){
-            next();
-        });
-    });
-
-    // setup prev buttons
-    $prevButtons.each(function(index){
-        $(this).on('click', function(){
-            prev();
-        });
-    });
-
-    
     function enablePlayButton() {
-        $pauseButton.css({display: 'none'});
-        $playButton.css({display: 'inline'});
+        $pauseButton.addClass(hiddenClass);
+        $playButton.removeClass(hiddenClass);
     }
 
     function enablePauseButton() {
-        $pauseButton.css({display: 'inline'});
-        $playButton.css({display: 'none'});
+        $pauseButton.removeClass(hiddenClass);
+        $playButton.addClass(hiddenClass);
     }
 
-    function playPauseEventHandler() {
-        if (isAutoPlay == true) {
+    function toggleAutoPlay() {
+        // turn off auto play
+        if (isAutoPlay) {
             isAutoPlay = false;
             clearTimeout(timeout);
-            enablePlayButton();            
-        } else if(isAutoPlay == false){
+            enablePlayButton();   
+            
+        // turn on auto play         
+        } else {
             isAutoPlay = true;
             enablePauseButton();
             advance();
         }
     }
+
+    function move(newIndex) {
+
+        if ($imageSet.is(':animated') || newIndex == currIndex) {
+            return;
+        }
+        else if (newIndex > currIndex) {       
+            $imageItems.eq(newIndex).removeClass(hiddenClass);
+            $imageItems.eq(newIndex).addClass(nextClass);
+            $imageSet.animate({right: '100%'}, slideDuration_ms, function(){
+                $imageSet.css({right: '', left: ''}); 
+                $imageItems.eq(newIndex).removeClass(nextClass);
+                $imageItems.eq(currIndex).addClass(hiddenClass);
+                $moveButons.eq(currIndex).removeClass(markedClass);
+                $moveButons.eq(newIndex).addClass(markedClass);
+                currIndex = newIndex;
+                nextIndex = (currIndex+1)%nImageItems;
+                prevIndex = ((currIndex-1)%nImageItems + nImageItems)%nImageItems;
+            });
+        } else if (newIndex < currIndex) {
+            $imageItems.eq(newIndex).removeClass(hiddenClass);
+            $imageItems.eq(newIndex).addClass(prevClass);
+            $imageSet.animate({left: '100%'}, slideDuration_ms, function(){
+                $imageSet.css({right: '', left: ''}); 
+                $imageItems.eq(newIndex).removeClass(prevClass);
+                $imageItems.eq(currIndex).addClass(hiddenClass);
+                $moveButons.eq(currIndex).removeClass(markedClass);
+                $moveButons.eq(newIndex).addClass(markedClass);
+                currIndex = newIndex;
+                nextIndex = (currIndex+1)%nImageItems;
+                prevIndex = ((currIndex-1)%nImageItems + nImageItems)%nImageItems;
+            });
+        }  
+        
+        if (isAutoPlay)
+        {
+            advance();
+        }
+    }
+
+    // setup inital move button marked state
+    $moveButons.eq(currIndex).addClass(markedClass);
+
+    //setup initial image visibility
+    $imageItems.filter(':not(:eq(' + currIndex + '))').addClass(hiddenClass);
     
-    // setup play/pause buttons
+    // setup each move button
+    $moveButons.each(function(index){
+        $(this).on('click', function(){move(index);});
+    });
+
+    // setup button call backs
+    $nextButtons.each(function(){$(this).on('click', next);});
+    $prevButtons.each(function(){$(this).on('click', prev);});  
+    $playButton.each(function(){$(this).on('click', toggleAutoPlay)});
+    $pauseButton.each(function(){$(this).on('click', toggleAutoPlay)});
+
+    // setup initial play/pause button visibility
     if (isAutoPlay == true) {
         enablePauseButton();
     } else if (isAutoPlay == false) {
         enablePlayButton();
     }
 
-    $playButton.each(function(){
-        $(this).on('click', playPauseEventHandler)
-    });
-
-    $pauseButton.each(function(){
-        $(this).on('click', playPauseEventHandler)
-    });
-
+    // being auto play
     if (isAutoPlay == true) {
         advance();
     }
