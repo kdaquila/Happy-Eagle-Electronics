d62b26001c3999b03a157b5eb39f3cee07ae1cbc
Kenneth D'Aquila <kfd182@gmail.com>
2019-05-10 23:37:37 -0500

    Refactored DropDown Nav Menu. Changed ID to Class.

--- js/dropDownMenu.js
+++ js/dropDownMenu.js
@@ -1,6 +1,5 @@
 $('body').on('click', function(){
-    var menu = document.getElementById("header-navmenu");
-    closeNavMenu(menu);
+    $('.headerTertiaryNav').each(closeNavMenu);
 });
 $('.headerSecondaryNav button').on('click', toggleNavMenu);
 $('.headerSecondaryNav button').on('click', function(e){e.stopPropagation();});
@@ -8,30 +7,27 @@ $('.headerSecondaryNav button').on('click', function(e){e.stopPropagation();});
 
 function toggleNavMenu()
 {
-    var menu = document.getElementById("header-navmenu");
-    var styles = getComputedStyle(menu);
-    if(styles.display != "none")
+    var displaySetting = $('.headerTertiaryNav').css("display");
+    if(displaySetting != "none")
     {
-        closeNavMenu(menu);
+        closeNavMenu();
     }
     else
     {
-        openNavMenu(menu);
+        openNavMenu();
     }
 }
 
-function openNavMenu(menu) {
-    menu.style.display = "flex"; 
-    menu.style.right = 0;
-    
+function openNavMenu() {
+    $('.headerTertiaryNav').css({display: "flex", right: "0"});    
     var $menuButton = $('.headerSecondaryNav button');
     $menuButton.css({backgroundColor:"rgb(240,240,240)"});
     $menuButton.css({"border":"dashed rgb(100,100,100) 2px"});
     $menuButton.css({zIndex:"10"});
 }
 
-function closeNavMenu(menu) {
-    menu.style.display = "none";
+function closeNavMenu() {
+    $('.headerTertiaryNav').css({display: "none"});
     var $menuButton = $('.headerSecondaryNav button');
     $menuButton.css({backgroundColor:"white"});
     $menuButton.css({"border":"dashed transparent 2px"});
